36f42c01c817c9aa91456f9e31328e8eea1713de
Jessica Arrivabem <jessica.arrivabem@gmail.com>
2025-02-19 08:37:13 -0300

    Screen Wraparound

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,21 +19,9 @@ public class PlayerMovement : MonoBehaviour
     }
     void Update()
     {
-        if (Touchscreen.current.primaryTouch.press.isPressed)
-        {
-           Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+        ProccessInput();
 
-           Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
-
-            movementDirection = transform.position - worldPosition; // vector pointing from where we touched to the player
-            movementDirection.z = 0f; // avoid the player to leave the screen on this axis
-            movementDirection.Normalize(); // maintains the direction while touching the screen
-
-        }
-        else
-        {
-            movementDirection = Vector3.zero; // when not touching the screen slow down the movement
-        }       
+        KeepPlayerOnScreen();
     }
 
     private void FixedUpdate() // everytime the physics system updated. Stays consistent regardless of the game's performance
@@ -44,5 +32,50 @@ public class PlayerMovement : MonoBehaviour
 
         rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity); // limite the velocity
 
+    }
+
+    private void ProccessInput()
+    {
+        if (Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+
+            Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+            movementDirection = transform.position - worldPosition; // vector pointing from where we touched to the player
+            movementDirection.z = 0f; // avoid the player to leave the screen on this axis
+            movementDirection.Normalize(); // maintains the direction while touching the screen
+
+        }
+        else
+        {
+            movementDirection = Vector3.zero; // when not touching the screen slow down the movement
+        }
+    }
+
+    private void KeepPlayerOnScreen()
+    {
+        Vector3 newPosition = transform.position;
+        Vector3 viewportPosition =  mainCamera.WorldToViewportPoint(transform.position);
+
+        if(viewportPosition.x > 1)
+        {
+            newPosition.x = -newPosition.x + 0.1f; // goes to right side of the screen, teleport to the other side and moves slight to right
+        }
+        else if(viewportPosition.x < 0)
+        {
+            newPosition.x = -newPosition.x - 0.1f;
+        }
+
+        if (viewportPosition.y > 1)
+        {
+            newPosition.y = -newPosition.y + 0.1f;
+        }
+        else if (viewportPosition.y < 0)
+        {
+            newPosition.y = -newPosition.y - 0.1f;
+        }
+
+        transform.position = newPosition;
     }
 }
